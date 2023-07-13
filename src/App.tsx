667af302933352b1b192f71644fbbe01e5578d8c
Facundo Ledesma <facundo.ledesma@Facundos-MBP.wind3.hub>
2023-07-13 12:37:12 +0200

    feat: add postcss, autoprefix & removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./app.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
 }
 
 export default App;
