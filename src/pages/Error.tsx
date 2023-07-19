23eb719d64752db02df3302a2e2fcbf3690a7920
Facundo Ledesma <facundo.ledesma@Facundos-MBP.wind3.hub>
2023-07-19 21:52:03 +0200

    feat: error page implementation

---
+++ src/pages/Error.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/buttons/Button';
+import { useQueryParam } from '@/hooks/useQueryParams';
+import MozioErrorPNG from '@/assets/images/mozio-error.png';
+
+interface IErrorProps {
+  message?: string;
+}
+
+export default function Error({
+  message = ''
+}: IErrorProps) {
+  const { searchParams } = useQueryParam();
+
+  return (
+    <div className="flex w-[734px] flex-col items-center justify-center gap-4 overflow-auto rounded-3xl border border-purple-light bg-blur py-[66px] shadow-purple-light">
+      <div className="flex items-center justify-center">
+        <div className="flex w-[30%] flex-col items-start gap-10">
+          <p className="text-bold w-[200px] text-3xl">
+            Oops! Something went wrong
+          </p>
+          <p className="text-bold">{message}</p>
+          <div className="flex flex-col gap-4">
+            <Link
+              to={{
+                pathname: '/home',
+                search: searchParams.toString()
+              }}
+            >
+              <Button label="Back" />
+            </Link>
+          </div>
+        </div>
+        <img src={MozioErrorPNG} alt="error-mozio" />
+      </div>
+    </div>
+  );
+}
