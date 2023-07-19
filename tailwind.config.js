c8d1a06dea3a18a0639d94815ca8d58915eed027
Facundo Ledesma <facundo.ledesma@Facundos-MBP.wind3.hub>
2023-07-19 21:43:44 +0200

    feat: update project config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Arial', 'sans-serif']
+      },
+      colors: {
+        black: 'var(--black)',
+        white: 'var(--white)',
+        blur: 'var(--blur)',
+        purple: {
+          light: 'var(--purple-light)',
+          dark: 'var(--purple-dark)',
+          darker: 'var(--purple-darker)'
+        },
+        gray: {
+          light: 'var(--gray)',
+          dark: 'var(--gray-dark)'
+        },
+        red: 'var(--red)'
+      },
+      boxShadow: {
+        'purple-lg': '0px 4px 6px -2px var(--purple-light)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
