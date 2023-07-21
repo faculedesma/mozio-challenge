0d6a835cc95b4ae45f1012280de7ad557991f76e
Facundo Ledesma <facundo.ledesma@Facundos-MBP.wind3.hub>
2023-07-21 18:06:07 +0200

    feat: add spinner, add rest props to buttons, rename query parm ext

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,5 +1,8 @@
-interface IButtonProps {
-  label?: string;
+import { ButtonHTMLAttributes } from 'react';
+
+interface IButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
   disabled?: boolean;
   onClick?: () => void;
 }
@@ -7,13 +10,15 @@ interface IButtonProps {
 export const Button = ({
   label = 'Button',
   disabled = false,
-  onClick = () => null
+  onClick = () => null,
+  ...restProps
 }: IButtonProps) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className="flex h-[38px] w-[335px] items-center justify-center rounded bg-purple-dark  px-[12px] py-[8px] text-white transition-all duration-300 hover:bg-purple-darker disabled:bg-gray-light md:w-auto"
+      {...restProps}
     >
       {label}
     </button>
--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -1,4 +1,7 @@
-interface IIconButtonProps {
+import { ButtonHTMLAttributes } from 'react';
+
+interface IIconButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   disabled?: boolean;
   onClick?: () => void;
@@ -7,13 +10,15 @@ interface IIconButtonProps {
 export const IconButton = ({
   children,
   disabled = false,
-  onClick = () => null
+  onClick = () => null,
+  ...restProps
 }: IIconButtonProps) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className={`border-gray flex h-[22px] w-[22px] items-center justify-center rounded bg-purple-light  px-2 py-3 text-white transition-all duration-300 hover:bg-purple-dark disabled:bg-gray-dark`}
+      {...restProps}
     >
       {children}
     </button>
