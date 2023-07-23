b1b26cc8190b5d571aa2becba84fa1652064b64d
Facundo Ledesma <facundo.ledesma@Facundos-MBP.wind3.hub>
2023-07-23 03:01:48 +0200

    fix: align center error page on mobile

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -15,11 +15,13 @@ export default function Error({
   return (
     <div className="flex w-[380px] items-center justify-center gap-4 overflow-auto rounded-3xl border border-purple-light bg-blur px-[22px] py-[30px] shadow-purple-light md:w-[734px] md:flex-col md:py-[66px]">
       <div className="flex flex-col-reverse items-center justify-center gap-10 md:flex-row">
-        <div className="flex flex-col items-start gap-10 md:w-[30%]">
+        <div className="flex flex-col items-center gap-10 md:w-[30%] md:items-start">
           <p className="text-bold text-3xl md:w-[200px]">
             Oops! Something went wrong
           </p>
-          <p className="text-bold">{message}</p>
+          <p className="text-bold text-center  md:text-start">
+            {message}
+          </p>
           <div className="flex flex-col gap-4">
             <Link
               to={{
